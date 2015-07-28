410a0e9fdd89e62210b5e9e99b53ec588f7a05c2
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-07-28 11:59:46 +0200

    basic setup

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('App: File Start');
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
+
+console.log('App: File End');
